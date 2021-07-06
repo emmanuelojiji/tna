9bcf2d44ea68f4d725efee314d54a4672f5b676f
emmanuelojiji <85620559+emmanuelojiji@users.noreply.github.com>
2021-07-06 07:08:13 +0100

    Add files via upload

--- books.js
+++ books.js
@@ -1,13 +1,22 @@
 window.onload = () => {
     const scrollLeft = document.querySelector("#scroll-left");
     const main = document.querySelector("#books-films-wrap");
-    const scrollRight = document.querySelector("#scroll-right");
+    const scrollRight = document.getElementsByClassName("scroll-right");
 
-    scrollRight.onclick = () => {
+    for (let index = 0; index < scrollRight.length; index++) {
+
+    scrollRight[index].onclick = () => {
             main.style.transform = "translateX(-100%)"
+            console.log("hey")
     }
+
+}
+
+
 
     scrollLeft.onclick = () => {
         main.style.transform = "translateX(0%)"
     }
+
+    
 }
